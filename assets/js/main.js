85fdf9315ff4cf532cd6db5115f72ac5ed3b8cc8
Sontos Sharma <sontossharma926@gmail.com>
2021-08-28 12:33:17 +0600

    update skill section

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,3 +29,60 @@ $('.single-image').magnificPopup({
 });
 // magnific popup end
 
+// chart start
+window.onload = function () {
+  var chartTitle = document.getElementById("chart-title").value;
+  var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    title:{
+      text: chartTitle
+    },
+    axisY: {
+      title: "Completed Projects",
+      valueFormatString: "#0",
+      suffix: "Projects",
+      stripLines: [{
+        value: 192,
+        label: "Average"
+      }]
+    },
+    data: [{
+      yValueFormatString: "#,### Projects",
+      xValueFormatString: "YYYY",
+      type: "spline",
+      dataPoints: [
+        {x: new Date(2016, 0), y: 126},
+        {x: new Date(2017, 0), y: 158},
+        {x: new Date(2018, 0), y: 204},
+        {x: new Date(2019, 0), y: 210},
+        {x: new Date(2020, 0), y: 150},
+        {x: new Date(2021, 0), y: 256}
+      ]
+    }]
+  });
+  chart.render();
+
+  }
+// chart end
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1500 || document.documentElement.scrollTop > 1800) {
+    document.getElementById("progress-bar1").style.width = "95%";
+    document.getElementById("progress-bar2").style.width = "98%";
+    document.getElementById("progress-bar3").style.width = "99%";
+    document.getElementById("progress-bar4").style.width = "96%";
+    document.getElementById("progress-bar5").style.width = "80%";
+    document.getElementById("progress-bar6").style.width = "90%";
+    document.getElementById("progress-bar7").style.width = "100%";
+  } else {
+    document.getElementById("progress-bar1").style.width = "0%";
+    document.getElementById("progress-bar2").style.width = "0%";
+    document.getElementById("progress-bar3").style.width = "0%";
+    document.getElementById("progress-bar4").style.width = "0%";
+    document.getElementById("progress-bar5").style.width = "0%";
+    document.getElementById("progress-bar6").style.width = "0%";
+    document.getElementById("progress-bar7").style.width = "0%";
+  }
+}
+
